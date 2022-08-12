ae8eea64b30cbd58c265fca65ada1fb9768c6ab4
Azamat Gaparov <agaparov@relaynetwork.com>
2022-08-12 13:27:02 -0400

    Add default imports and form submit event listener

---
+++ script.js
@@ -0,0 +1,17 @@
+const balanceAmount = document.getElementById('balance');
+const moneyPlusAmount = document.getElementById('money-plus');
+const moneyMinusAmount = document.getElementById('money-minus');
+const historyList = document.getElementById('list');
+const addTransactionForm = document.getElementById('form');
+const newTransactionText = document.getElementById('text');
+const newTransactionAmount = document.getElementById('amount');
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+  // Add your code
+}
+
+// Add your code
+
+addTransactionForm.addEventListener('submit', addTransaction);
